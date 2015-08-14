6161394c642c957f3627cacc704f1275b6629e9f
ivoszz <ivoszz@gmail.com>
2015-08-14 16:38:38 +0200

    Add TimePassed component

--- app/index.js
+++ app/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import HelloForm from 'components/helloForm';
 import Timer from 'components/timer';
+import TimePassed from 'components/timePassed';
 
-React.render(<div><HelloForm /><Timer /></div>, document.getElementById('content'));
+React.render(
+  <div>
+    <HelloForm />
+    <Timer />
+    <TimePassed time='2015-08-12 08:45' />
+  </div>,
+  document.getElementById('content')
+);
